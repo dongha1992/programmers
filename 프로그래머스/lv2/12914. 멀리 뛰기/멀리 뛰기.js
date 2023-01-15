5a04cda5180d2fca242c30f43498a7b4f94c9a69
김동하 <58624851+dongha1992@users.noreply.github.com>
2023-01-15 21:42:57 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = 0;
+  let dy = Array.from({length:n+1}, ()=>0)
+  const memo = {}
+  dy[1] = 1
+  dy[2] = 2
+  
+  for(let i = 3; i <= n; i++){
+    dy[i] = (dy[i-2] + dy[i-1]) % 1234567;
+  }
+  return dy[n] 
+}
