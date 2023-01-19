717566d9382577fbaff029e05665f35ca2be9e80
김동하 <58624851+dongha1992@users.noreply.github.com>
2023-01-19 15:01:12 +0900

    [level 2] Title: 문자열 압축, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,28 @@
+function solution(s) {
+ let answer = Number.MAX_SAFE_INTEGER
+  
+ const pressStr = (str, k) => {
+   let tmp = ""
+   let cnt = 1;
+   for(let i = 0; i < str.length; i+=k){
+    const cur = str.substring(i, i+k)
+    const next = str.substring(i+k, i+(2*k))
+
+    if(cur === next) cnt++
+    else {
+      tmp += cur
+      if(cnt > 1) tmp+= String(cnt)
+      cnt=1
+    }
+   }
+   return tmp.length
+ }
+ 
+ for(let i = 1; i <= s.length; i++){
+   if(answer >= pressStr(s, i)){
+     answer = pressStr(s, i)
+   }
+}
+
+ return answer
+}
