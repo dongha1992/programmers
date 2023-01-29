0270d9fe1607db5e188a953281c44c406d9871ab
김동하 <58624851+dongha1992@users.noreply.github.com>
2023-01-29 17:59:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.27 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,5 @@
 function solution(numbers, target) {
+
   const len = numbers.length
   const ch = Array.from({length:len}, ()=>0)
   let answer= 0;
@@ -8,15 +9,14 @@ function solution(numbers, target) {
     if(L===len){
       if(sum === target) answer++
     } else {
-      if(ch[L]===0){
         ch[L] = 1
         dfs(L+1, sum+numbers[L])
         ch[L] = 0
         dfs(L+1, sum-numbers[L])
-      }
     }
   }
   dfs(0, 0)
   
   return answer
+
 }
