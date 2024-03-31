2a76dbf0a41b2546d26c0183c12293a1b0cad1b0
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-04-01 02:13:30 +0900

    [level 1] Title: 공원 산책, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,107 +1,101 @@
 function solution(park, routes) {
- 
-  const parks = park.map(p=>p.split(""))
-  const routeArray = routes.map(r => r.split(" "))
+   
+const parks = park.map((p) => p.split(''));
+const routeArray = routes.map((r) => r.split(' '));
 
-  const n = park.length
-  const m = routeArray.length
-  
-  let currentPos = []
+const n = park.length;
+const m = routeArray.length;
 
-  // console.log(parks)
-  for(let i = 0; i < n; i++){
-    for(let j =0; j < parks[i].length; j++){      
-      if(parks[i][j] === 'S'){
-        currentPos = [i, j]
-        for(let k = 0; k < m; k++){
-          const directions = routeArray[k]
-          const [direction, count] = directions
-          
-          if(direction === 'N') {
-            const nxI = currentPos[0] - Number(count)
-            const curJ = currentPos[1] 
-            
-            const check = (curI, nxI, curJ) => {
-              let flag = false
-              for(let l = curI; l >= nxI; l--){ 
-                if(parks[l][curJ] === 'X' || !parks[l][curJ]){
-                  flag = true
-                  break;
-                }
-                
-              }
-               return flag
-            }
-            if(nxI >= n || nxI < 0) continue;
-            
-            if(parks[nxI][curJ]==='X' || check(currentPos[0], nxI,curJ)){
-              continue;
-            }
-            currentPos = [nxI, curJ]
+let currentPos = [];
 
-          } else if(direction === 'S'){
-            const nxI = currentPos[0] + Number(count)
-            const curJ = currentPos[1] 
-            const check = (curI, nxI, curJ) => {
-              let flag = false
-              for(let l = curI; l <= nxI; l++){ 
-                if(parks[l][curJ] === 'X' || !parks[l][curJ]){
-                  flag = true
-                  break;
-                }
-              }
-               return flag
-            }
-    
-            if(nxI >= n || nxI < 0) continue;
-          
-            if(parks[nxI][curJ]==='X' ||  check(currentPos[0],nxI, curJ)){
-              continue;
-            }
-            currentPos = [nxI, curJ]
-          } else if(direction === 'W'){
-            const curI = currentPos[0] 
-            const nxJ = currentPos[1] - Number(count)
-            const check = (curJ, curI, nxJ) => {
-              let flag = false
-              for(let l = curJ; l >= nxJ; l--){ 
-                if(parks[curI][l] === 'X' || !parks[curI][l]){
-                  flag = true
-                  break;
-                }
-              }
-               return flag
-            }
-            if(nxJ >= m || nxJ < 0) continue;
-            
-            if(parks[curI][nxJ]==='X'|| check(currentPos[1], curI, nxJ)){
-              continue;
-            } 
-            currentPos = [curI, nxJ]
-           } else if(direction=== 'E'){
-            const curI = currentPos[0] 
-            const nxJ = currentPos[1] + Number(count)
-            const check = (curJ, nxI, nxJ) => {
-               let flag = false
-              for(let l = curJ; l <= nxJ; l++){ 
-                if(parks[nxI][l] === 'X' || !parks[nxI][l]){
-                  flag = true
-                  break;
-                }
-              }
-               return flag
-            }
-            if(nxJ > m || nxJ < 0) continue;
-            
-            if(parks[curI][nxJ]==='X' || check(currentPos[1],curI ,nxJ)){
-              continue;
-            }
-            currentPos = [curI, nxJ]
-           }
-        } 
-        break;
-      }
+const checkColumn = (start, end, fixed) => {
+  let flag = false;
+  for (let l = start; l >= end; l--) {
+    if (parks[l][fixed] === 'X' || !parks[l][fixed]) {
+      flag = true;
+      break;
     }
   }
-  return currentPos
+  return flag;
+};
+  
+const checkRow = (start, end, fixed) => {
+  let flag = false;
+  for (let l = start; l >= end; l--) {
+    if (parks[fixed][l] === 'X' || !parks[fixed][l]) {
+      flag = true;
+      break;
+    }
+  }
+  return flag;
+};
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < parks[i].length; j++) {
+    if (parks[i][j] === 'S') {
+      currentPos = [i, j];
+      for (let k = 0; k < m; k++) {
+        const directions = routeArray[k];
+        const [direction, count] = directions;
+
+        if (direction === 'N') {
+          const nxI = currentPos[0] - Number(count);
+          const curJ = currentPos[1];
+          
+          const isLimit = nxI >= n || nxI < 0
+          if (isLimit || parks[nxI][curJ] === 'X' || checkColumn(currentPos[0], nxI, curJ)) {
+            continue;
+          }
+          currentPos = [nxI, curJ];
+        } else if (direction === 'S') {
+          const nxI = currentPos[0] + Number(count);
+          const curJ = currentPos[1];
+    
+          const isLimit = nxI >= n || nxI < 0
+          
+          if (isLimit || parks[nxI][curJ] === 'X' || checkColumn(nxI,currentPos[0], curJ)) {
+            continue;
+          }
+          currentPos = [nxI, curJ];
+        } else if (direction === 'W') {
+          const curI = currentPos[0];
+          const nxJ = currentPos[1] - Number(count);
+
+          const isLimit = nxJ >= m || nxJ < 0
+          
+          if (
+           isLimit || parks[curI][nxJ] === 'X' || checkRow(currentPos[1], nxJ, curI)
+          ) {
+            continue;
+          }
+          currentPos = [curI, nxJ];
+        } else if (direction === 'E') {
+          const curI = currentPos[0];
+          const nxJ = currentPos[1] + Number(count);
+          const check = (curJ, nxI, nxJ) => {
+            let flag = false;
+            for (let l = curJ; l <= nxJ; l++) {
+              if (parks[nxI][l] === 'X' || !parks[nxI][l]) {
+                flag = true;
+                break;
+              }
+            }
+            return flag;
+          };
+  
+          const isLimit = nxJ > m || nxJ < 0
+          const canCheck = currentPos[1] >= 0 && curI > 0 && nxJ > 0;
+          if (
+           isLimit|| parks[curI][nxJ] === 'X' ||
+         checkRow(nxJ,currentPos[1], curI)) {
+            continue;
+          }
+          currentPos = [curI, nxJ];
+        }
+      }
+      break;
+    }
+  }
+}
+return currentPos;
 }
