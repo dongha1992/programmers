170cd0ff2ee0e8cfb2872721146c578c3629d2a0
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-04-07 16:01:02 +0900

    Update ［PCCP 기출문제］ 1번 ／ 붕대 감기.js

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,31 +1,56 @@
+
 function solution(bandage, health, attacks) {
   const attackMap = new Map(attacks);
   const [castingTime, recoveryAmount, extraRecovery] = bandage;
   const lastAttackSec = attacks[attacks.length - 1][0];
   const maxHealth = health;
-  let serialRecovery = -1
+  let serialRecovery = -1;
 
   for (let sec = 0; sec <= lastAttackSec; sec++) {
     const isAttacked = attackMap.has(sec);
     if (isAttacked) {
       const damage = attackMap.get(sec);
-      health = health - damage;
-      serialRecovery = 0;
-      if(health <= 0){
-        return -1
+      health = damagedHelath(health, damage);
+      serialRecovery = resetSerialRecovery();
+      if (checkIsDead(health)) {
+        return -1;
       }
     } else {
-      serialRecovery++;
-      health += recoveryAmount;
-      if (serialRecovery === castingTime) {
-        health += extraRecovery 
-        serialRecovery = 0;
+      health = recoveryHelath(health, recoveryAmount);
+      serialRecovery = increaseSerialRecovery(serialRecovery);
+      if (canGetExtraRecovery(serialRecovery, castingTime)) {
+        health = recoveryHelath(health, extraRecovery);
+        serialRecovery = resetSerialRecovery();
       }
     }
-     if (health >= maxHealth) {
-        health = maxHealth;
-      }
-    console.log(health, serialRecovery);
+    health = initHelath(health, maxHealth);
   }
-  return health <= 0 ? -1 : health
+  return health;
 }
+
+const initHelath = (health, maxHealth) => {
+  return health >= maxHealth ? maxHealth : health;
+};
+const recoveryHelath = (helath, recoveryAmount) => {
+  return helath + recoveryAmount;
+};
+
+const increaseSerialRecovery = (serialRecovery) => {
+  return serialRecovery + 1;
+};
+
+const checkIsDead = (health) => {
+  return health <= 0;
+};
+
+const resetSerialRecovery = () => {
+  return 0;
+};
+
+const damagedHelath = (helath, damage) => {
+  return helath - damage;
+};
+
+const canGetExtraRecovery = (serialRecovery, castingTime) => {
+  return serialRecovery === castingTime;
+};
