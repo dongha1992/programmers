2b1f8c2cc1020b45846c3eb5cfb3cc5524338dbb
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-04-17 16:43:15 +0900

    [level 2] Title: 땅따먹기, Time: 20.30 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,15 @@
+function solution(land) {
+    let max = 0;
+    
+    for (let i = 1; i < land.length; i++) {
+        land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
+        land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
+        land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
+        land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
+    }
+    
+    max = Math.max(...land[land.length - 1]);
+    
+    return max;
+
+}
