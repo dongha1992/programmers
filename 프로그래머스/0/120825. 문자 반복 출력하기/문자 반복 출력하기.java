9014bd14906077227883e82f949f12c1822f0765
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-06-16 21:44:15 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 11.22 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        for(char str : my_string.toCharArray()){
+            for(int i = 0; i < n; i++){
+                answer += str;
+            }
+        }
+        return answer;
+    }
+}
