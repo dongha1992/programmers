4d6418fc81024989dd4d7682bc7c268d995ab254
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-08-03 21:27:26 +0900

    [level 3] Title: 입국심사, Time: 62.48 ms, Memory: 40.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,28 @@
+function solution(n, times) {
+  times.sort((a, b) => a - b);
+  
+  let low = 1;
+  let high = n * times[times.length -1];
+  let answer = high;
+  
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    let sum = 0;
+    
+    for (let i = 0; i < times.length; i++) {
+      sum += Math.floor(mid / times[i]);
+      if (sum >= n) {
+          break;
+      }
+    }
+    
+    if (sum >= n) {
+      answer = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  
+  return answer;  
+}
