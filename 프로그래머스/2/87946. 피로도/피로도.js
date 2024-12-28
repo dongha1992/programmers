133fbbfc2c09dffaf340fcdc13b45b2058f35027
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-12-28 22:18:01 +0900

    [level 2] Title: 피로도, Time: 34.89 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,23 +1,21 @@
 function solution(k, dungeons) {
-     const len = dungeons.length;
-  const ch = Array.from({ length: len }, () => 0);
-  let answer = Number.MIN_SAFE_INTEGER;
-  let sum = 0;
-
-  const dfs = (L, sum) => {
-    for (let i = 0; i < dungeons.length; i++) {
-      const minimum = dungeons[i][0];
-      if (ch[i] === 0 && sum >= minimum) {
-        ch[i] = 1;
-        dfs(i + 1, sum - dungeons[i][1]);
-        ch[i] = 0;
-      }
-    }
-    const cnt = ch.filter(v => v !== 0).length;
-    if (answer < cnt) answer = cnt;
-  };
+    const n = dungeons.length;
+    const ch = Array.from({length:n}, ()=>0)
+    let answer = 0;
     
-
-  dfs(0, k);
-  return answer;
-}
+    const dfs = (l, sum) => {
+        for(let i = 0; i < n; i++) {
+            const [need, power] = dungeons[i]
+            if(ch[i] === 0 && sum >= need){
+                ch[i] = 1;
+                dfs(i + 1, sum - power);
+                ch[i] = 0;
+            }
+            const cnt = ch.filter(v => v !== 0).length;
+            if (answer < cnt) answer = cnt;
+        }
+    }
+    
+    dfs(0, k)
+    return answer;
+  }
