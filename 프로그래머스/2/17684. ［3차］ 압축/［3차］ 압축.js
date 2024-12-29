232c54b8d2a9668be8e468f525f3a56b4729a787
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-12-29 21:17:39 +0900

    [level 2] Title: [3차] 압축, Time: 387.85 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,46 +1,52 @@
-
-const createAlphabetArray = () => {
-  const startCharCode = 'A'.charCodeAt(0);
-  const endCharCode = 'Z'.charCodeAt(0);
-  const alphabetArray = [];
-
-  for (let charCode = startCharCode; charCode <= endCharCode; charCode++) {
-    const char = String.fromCharCode(charCode);
-    alphabetArray.push(char);
-  }
-
-  return alphabetArray;
-};
-
-const getChar = (str, start, end) => {
-  return str.substring(start, end);
-};
-
-
 function solution(msg) {
+  const charMap = createCharMap();
   const answer = [];
-  const charMap = {};
   let startIndex = 0;
   let endIndex = msg.length;
 
-  createAlphabetArray().forEach((char, index) => {
-    charMap[char] = index + 1;
-  });
-
-  while (startIndex < msg.length) {
+  while (startIndex < endIndex) {
     const currentChar = getChar(msg, startIndex, endIndex);
-    if (!charMap[currentChar]) {
-      endIndex--;
-    } else {
-      answer.push(charMap[currentChar]);
-      const nextChar = currentChar + getChar(msg, endIndex, endIndex + 1);
-      const size = Object.values(charMap).length;
-      charMap[nextChar] = size + 1;
-      startIndex = endIndex;
-      endIndex = msg.length;
-    }
-  }
+    const foundChar = charMap[currentChar];
 
+    if (!foundChar) {
+      endIndex--;
+      continue;
+    } 
+    
+    answer.push(foundChar);
+    const nextChar = currentChar + getChar(msg, endIndex, endIndex + 1);
+    const size = getMapSize(charMap);
+    charMap[nextChar] = size + 1;
+    startIndex = endIndex;
+    endIndex = msg.length;
+  }
+  
   return answer;
 }
+
+
+const createAlphabet = () => {
+  const startCode = 'A'.charCodeAt(0);
+  const endCode = 'Z'.charCodeAt(0);
+  const result = [];
+
+  for (let i = startCode; i <= endCode; i++) {
+    const char = String.fromCharCode(i);
+    result.push(char);
+  }
+  return result;
+};
+
+const createCharMap = () =>
+  createAlphabet().reduce((map, char, index) => {
+    map[char] = index + 1;
+    return map;
+  }, {});
+
+const getMapSize = (charMap) => {
+  return Object.values(charMap).length;
+};
+
+const getChar = (str, start, end) => str.substring(start, end);
+
 
