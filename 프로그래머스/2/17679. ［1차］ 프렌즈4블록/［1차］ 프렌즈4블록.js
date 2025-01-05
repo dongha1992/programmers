2243844c6e5629dbac49eb5f25c2bf7b704add99
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-05 19:52:13 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 26.37 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,91 +1,119 @@
+function solution(m, n, board) {
+  let answer = 0;
+  let checked = generateCheckedArr();
 
-const solution = (m, n, board) => {
-const checked = Array.from({ length: m }, () => new Array(n).fill(1));
-  board = board.map(b=>b.split(""))
-  let finishCnt = 0
-  const dx = [0, 1, 1]
-  const dy = [1, 1, 0]
+  board = board.map((b) => b.split(''));
 
+  const dx = [1, 1, 0];
+  const dy = [0, 1, 1];
 
-
-const reorderBoard = (board) => {
-  let resultBoard = board
-  
-    for(let col = 0; col < n; col++){
-     for(let row = m-1; row >= 0; row--){
-      let startIndex = row
-       if(row > 1 && resultBoard[row][col] === 0){
-           while(true) {
-             let temp = resultBoard[startIndex-1][col]
-            resultBoard[startIndex-1][col] = resultBoard[startIndex][col]
-            resultBoard[startIndex][col] = temp
-        
-           let checTemp = checked[startIndex-1][col]
-           checked[startIndex-1][col] = checked[startIndex][col]
-           checked[startIndex][col] = checTemp  
-           startIndex--
-           if(startIndex <= 0) break;
-         }
-       } 
-    }
-  } 
-  
-   return resultBoard
-}
-
-const changeBoard = (board) => {
-  const resultBoard = board
-  for(let row = 0; row < m; row++){
-    for(let col = 0; col < n; col++){
-      if(!checked[row][col]){
-        board[row][col]= 0
-      }
-    }
-  }
-  return resultBoard
-}
-
-const changeCheckedArr = (pos) => {
-  const [current, right, rightBottom, bottom] = pos;
-  checked[current[0]][current[1]] = 0
-  checked[right[0]][right[1]] = 0
-  checked[rightBottom[0]][rightBottom[1]] = 0
-  checked[bottom[0]][bottom[1]] = 0
-}
-
-const checkBoard = (board) => {
-  let resultBoard = board
-   for(let row = 0; row < m-1; row++){
-    for(let col = 0; col < n-1; col++){
-      let flag = true
-      for(let k = 0; k < 3; k++){
-        const nx = row + dx[k]
-        const ny = col + dy[k]
-        if(nx >=0 && nx <= m && ny >=0 && ny <= n){
-         const current = resultBoard[row][col]
-         const target = resultBoard[nx][ny]
-         if(current !== target){
-           flag = false
-          }
-          if(k === 2 && flag){
-            changeCheckedArr([[row, col], [row, col+dy[0]],[row+dx[1], col+dy[1]],[row+dx[2], col]])
-          }
+  function checkBoard() {
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        if (board[i][j] !== '0') {
+          checkIsSame(i, j);
         }
       }
-      
-      finishCnt++
     }
   }
-  return resultBoard
+
+  function rotateBoard() {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+      let col = [];
+      for (let j = 0; j < m; j++) {
+        col.push(board[m - j - 1][i]);
+      }
+      arr.push(col);
+    }
+    return arr;
+  }
+
+  function rotateBackBoard(board) {
+    let arr = [];
+    for (let i = 0; i < m; i++) {
+      let col = [];
+      for (let j = 0; j < n; j++) {
+        col.push(board[j][m - i - 1]);
+      }
+      arr.push(col);
+    }
+    return arr;
+  }
+
+  function reorderBoard() {
+    const rotatedBoard = rotateBoard();
+    const filteredBoard = fillterArray(rotatedBoard);
+    const filledArray = fillArray(filteredBoard);
+    const rotateAsOrigin = rotateBackBoard(filledArray);
+
+    board = rotateAsOrigin;
+    checked = generateCheckedArr();
+  }
+
+  function fillterArray(arr) {
+    return arr.map((subArr) => subArr.filter((a) => a !== '0'));
+  }
+
+  function fillArray(arr) {
+    return arr.map((subArr) => [
+      ...subArr,
+      ...Array(m - subArr.length).fill('0'),
+    ]);
+  }
+
+  function changeBoard() {
+    for (let i = 0; i < checked.length; i++) {
+      for (let j = 0; j < checked[i].length; j++) {
+        if (checked[i][j] === 1) {
+          board[i][j] = '0';
+          answer++;
+        }
+      }
+    }
+  }
+
+  function changeChecked(blocks) {
+    for (let i = 0; i < blocks.length; i++) {
+      const [x, y] = blocks[i];
+      checked[x][y] = 1;
+    }
+  }
+
+  function checkIsSame(i, j) {
+    let currentBlock = board[i][j];
+    let blocks = [[i, j]];
+
+    for (let k = 0; k < 3; k++) {
+      const nx = i + dx[k];
+      const ny = j + dy[k];
+      if (nx < m && ny < n && nx >= 0 && ny >= 0) {
+        const nextBlock = board[nx][ny];
+        if (nextBlock !== currentBlock || nextBlock === '0') return;
+        blocks.push([nx, ny]);
+      }
+    }
+    changeChecked(blocks);
+  }
+
+  function generateCheckedArr() {
+    return Array.from({ length: m }, () => Array(n).fill(0));
+  }
+
+  function start() {
+    checkBoard();
+    changeBoard();
+    reorderBoard();
+  }
+
+ let prevAnswer = -1;
+    
+ while (prevAnswer !== answer) {
+  prevAnswer = answer;
+  start();
 }
 
 
-while(true){
-  board = reorderBoard(changeBoard(checkBoard(board)))
-  if(finishCnt > 380000) break;
+  return answer;
 }
-
-  return  m * n - checked.flatMap(arr => arr.filter(c=>c)).length
-};
-
 
