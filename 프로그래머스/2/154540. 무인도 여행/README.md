de1e1ecdd82e74c94b3dbd1ca1599c1602e94eed
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-06 22:01:47 +0900

    [level 2] Title: 무인도 여행, Time: 8.51 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/README.md
+++ 프로그래머스/2/154540. 무인도 여행/README.md
@@ -1,10 +1,10 @@
 # [level 2] 무인도 여행 - 154540 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.38 ms
+메모리: 37.9 MB, 시간: 8.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 12:14:02
+2025년 01월 06일 22:01:44
 
 ### 문제 설명
 
--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,37 +1,34 @@
-function solution(g) {
+function solution(maps) {
   const dx = [1, 0, -1, 0];
   const dy = [0, 1, 0, -1];
-  const n = g.length;
-  const m = g[0].length;
-  let ch = Array.from({ length: n }, () => Array.from({ length: m }).fill(0));
-  // ch[0][0] = 1;
+  const n = maps.length;
+  const m = maps[0].length;
+  const ch = Array.from({ length: n }, () => Array.from({ length: m }).fill(0));
   let answer = [];
-  let total = 0;
 
   const dfs = (i, j) => {
     ch[i][j] = 1;
-    let total = Number(g[i][j]); 
-    
+    let days = Number(maps[i][j]);
     for (let k = 0; k < 4; k++) {
       const nx = dx[k] + i;
       const ny = dy[k] + j;
+
       if (nx >= 0 && nx < n && ny >= 0 && ny < m && ch[nx][ny] === 0) {
-        if (g[nx][ny] !== 'X') {
-          total += dfs(nx, ny);
+        if (maps[nx][ny] !== 'X') {
+          days += dfs(nx, ny);
         }
       }
     }
-    return total
+    return days;
   };
 
-  for (let i = 0; i < g.length; i++) {
-    for (let j = 0; j < g[0].length; j++) {
-      if (g[i][j] !== 'X' && ch[i][j] === 0) {
-        const total = dfs(i, j);
-        answer.push(total);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (maps[i][j] !== 'X' && ch[i][j] === 0) {
+        const days = dfs(i, j);
+        answer.push(days);
       }
     }
   }
-    
-  return answer.length ? answer.sort((a, b) => a-b) : [-1]
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
 }
