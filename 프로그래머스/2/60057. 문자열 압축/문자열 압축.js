43846429bbe649c352b646bcb3fc1e1c4cebd928
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-19 15:32:22 +0900

    [level 2] Title: 문자열 압축, Time: 3.30 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -2,26 +2,27 @@
 function solution(s) {
   let answer = Number.MAX_SAFE_INTEGER;
 
-  const pressStr = (str, k) => {
-    let tmp = '';
+  const pressString = (str, k) => {
+    let result = "";
     let cnt = 1;
-    for (let i = 0; i < str.length; i += k) {
-      const cur = str.substring(i, i + k);
-      const next = str.substring(i + k, i + 2 * k);
-
-      if (cur === next) cnt++;
+    
+    for(let i = 0; i < str.length; i += k){
+      const cur = str.substring(i, i+k)
+      const next = str.substring(i + k, i + 2 * k)
+      if(cur === next) cnt++;
       else {
-        tmp += cur;
-        if (cnt > 1) tmp += String(cnt);
+        result += cur;
+        if(cnt > 1) result += String(cnt);
         cnt = 1;
       }
     }
-    return tmp.length;
-  };
+    return result.length;
+  }
 
-  for (let i = 1; i <= s.length; i++) {
-    if (answer >= pressStr(s, i)) {
-      answer = pressStr(s, i);
+  
+  for(let i = 1; i <= s.length; i++){
+    if(answer >= pressString(s, i)){
+      answer = pressString(s, i)
     }
   }
 
