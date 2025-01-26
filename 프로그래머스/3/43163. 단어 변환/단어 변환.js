708c673c25ac97f9c30ec353b684b5c6d872b22e
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-26 17:07:57 +0900

    [level 3] Title: 단어 변환, Time: 0.85 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,54 +1,48 @@
 function solution(begin, target, words) {
-const ch = Array.from({length:words.length}).fill(0)
   let answer = 0;
-  if(!words.includes(target)){return 0}
+  const ch = Array.from({length:words.length}).fill(0)
   
-   const bfs = (word) => {
-    const queue = []
+  if(!words.includes(target)) return 0
+  
+  const bfs = (word) => {
+    const queue = [];
     queue.push([word, 0])
-    let min = Infinity
     
-     while(queue.length){
-       let [currentWord, step] = queue.shift();
-       const cnt = countNonOverlapChars(currentWord, target)
-       if(cnt === 1){
-         step++
-         answer = step
-         return 
-       } 
-       if(currentWord === target) {
+    while(queue.length){
+      let [cur, step] = queue.shift();
+      const cnt = diffBetweenWord(cur, target);
+      
+      if(cur === target) {
          answer = step
          return
        }
-       for(let i = 0 ; i < words.length; i++){
-         if(ch[i]===0){
-          const w = words[i]
-          const cnt = countNonOverlapChars(currentWord, w)
+      
+      for(let i = 0; i < words.length; i++){
+        if(ch[i] === 0){
+          const word = words[i]
+          const cnt = diffBetweenWord(cur, word);
           if(cnt === 1){
-           step++
-           queue.push([w, step])
-           ch[i] = 1
+            step++;
+            queue.push([word, step]);
+            ch[i] = 1;
           } 
-         }
-       }
-     }  
-   }
-   
-   bfs(begin)
-  
-   return answer
+        }
+      }
+    }     
+  }
+  bfs(begin)
+  return answer
 }
 
-function countNonOverlapChars(str1, str2) {
-    let count = 0;
-    const minLength = Math.min(str1.length, str2.length);
-    
-    for (let i = 0; i < minLength; i++) {
-        if (str1[i] !== str2[i]) {
-            count++;
-        }
-    }
-    count += Math.abs(str1.length - str2.length);
-    
-    return count;
+  
+function diffBetweenWord(str1, str2) {
+  let cnt = 0;
+  const minLength = Math.min(str1.length, str2.length);
+  
+  for(let i = 0; i < minLength; i++){
+    if(str1[i] !== str2[i]) cnt++;
+  }
+  
+  cnt += Math.abs(str1.length - str2.length);
+  return cnt;
 }
