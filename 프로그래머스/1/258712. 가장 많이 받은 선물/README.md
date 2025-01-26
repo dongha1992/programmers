f4292fefcf999e552d4cba178e5401e42f9bdef1
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-26 19:34:05 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 41.15 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 34.3 MB, 시간: 2.95 ms
+메모리: 38.6 MB, 시간: 41.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 06일 17:44:14
+2025년 01월 26일 19:34:03
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,48 +1,39 @@
 function solution(friends, gifts) {
-const n = friends.length;
- const giftsGraph = Array.from({length:n}, ()=>Array.from({length:n}).fill(0))
- const giftsIndexes = [];
- const answer = new Map()
+ const n = friends.length;
+  const graph = Array.from({length:n}, ()=>Array.from({length:n}).fill(0))
+  const giftsIndexes = Array(n).fill(0);
+  const answer = new Map();
   
- for(let gift of gifts){
-   const [giver, taker] = gift.split(" ")
-   const giverIndex = friends.indexOf(giver)
-   const takerIndex = friends.indexOf(taker)
-   giftsGraph[giverIndex][takerIndex] = giftsGraph[giverIndex][takerIndex]+1
- }
-  
-  
-  for(let i = 0; i < giftsGraph.length; i++){
-    const totalGive = giftsGraph[i].reduce((acc, cur)=> acc+cur,0)
-    let totalTake = 0;
-    for(let j = 0; j < giftsGraph.length; j++){
-      totalTake += giftsGraph[j][i]
-    }
-    giftsIndexes.push(totalGive-totalTake)
+  for(let gift of gifts){
+    const [giver, taker] = gift.split(" ")
+    const g = friends.indexOf(giver)
+    const t = friends.indexOf(taker)
+    graph[g][t]++
+    giftsIndexes[g]++
+    giftsIndexes[t]--
   }
+
   
-  for(let i = 0; i < giftsGraph.length; i++){
-    
-    for(let j = 0;  j < giftsGraph.length; j++){
-      const give = giftsGraph[i][j]
-      const take = giftsGraph[j][i]
-      if(i!==j){
-        if(give < take){
-          if(answer.has(friends[j])){
-            answer.set(friends[j], answer.get(friends[j])+1)
+  for(let i = 0; i < graph.length; i++){
+    for(let j = 0; j < graph.length; j++){
+      if(i === j) continue;
+      const g = graph[i][j]
+      const t = graph[j][i]
+      
+      if(g < t){
+        if(answer.has(friends[j])){
+          answer.set(friends[j], answer.get(friends[j])+1)
           } else {
             answer.set(friends[j], 1)
           }
-        } else if(give === take && giftsIndexes[i] > giftsIndexes[j]){
+        } else if(g === t && giftsIndexes[i] > giftsIndexes[j]){
           if(answer.has(friends[i])){
             answer.set(friends[i], answer.get(friends[i])+1)
           } else {
             answer.set(friends[i], 1)
           }
-          
         }
       }
     }
-  }
-  return  Math.max(...answer.values()) > 0 ? Math.max(...answer.values()) : 0
+    return  Math.max(...answer.values()) > 0 ? Math.max(...answer.values()) : 0
 }
