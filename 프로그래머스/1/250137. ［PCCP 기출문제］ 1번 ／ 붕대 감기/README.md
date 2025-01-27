61ca87473c938fa354ed0bcd5ee3008674f31814
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-27 18:06:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.71 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 붕대 감기 - 250137 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.39 ms
+메모리: 33.7 MB, 시간: 0.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 07일 16:03:48
+2025년 01월 27일 18:06:23
 
 ### 문제 설명
 
--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,56 +1,50 @@
-
 function solution(bandage, health, attacks) {
-  const attackMap = new Map(attacks);
-  const [castingTime, recoveryAmount, extraRecovery] = bandage;
-  const lastAttackSec = attacks[attacks.length - 1][0];
-  const maxHealth = health;
-  let serialRecovery = -1;
+  const manager = new HealthManager(bandage, health, attacks);
 
-  for (let sec = 0; sec <= lastAttackSec; sec++) {
-    const isAttacked = attackMap.has(sec);
-    if (isAttacked) {
-      const damage = attackMap.get(sec);
-      health = damagedHelath(health, damage);
-      serialRecovery = resetSerialRecovery();
-      if (checkIsDead(health)) {
-        return -1;
-      }
+  for (let sec = 0; sec <= manager.lastAttackSec; sec++) {
+    if (manager.attackMap.has(sec)) {
+      manager.takeDamage(manager.attackMap.get(sec));
+      if (manager.isDead()) return -1;
     } else {
-      health = recoveryHelath(health, recoveryAmount);
-      serialRecovery = increaseSerialRecovery(serialRecovery);
-      if (canGetExtraRecovery(serialRecovery, castingTime)) {
-        health = recoveryHelath(health, extraRecovery);
-        serialRecovery = resetSerialRecovery();
-      }
+      manager.recover();
     }
-    health = initHelath(health, maxHealth);
+    manager.capHealth();
   }
-  return health;
+  return manager.health;
+};
+
+class HealthManager {
+  constructor(bandage, maxHealth, attacks) {
+    [this.castingTime, this.recoveryAmount, this.extraRecovery] = bandage;
+    this.maxHealth = maxHealth;
+    this.health = maxHealth;
+    this.attackMap = new Map(attacks);
+    this.lastAttackSec = attacks[attacks.length - 1][0];
+    this.serialRecovery = 0;
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+    this.serialRecovery = 0;
+  }
+
+  recover() {
+    this.health += this.recoveryAmount;
+    this.serialRecovery++;
+    if (this.serialRecovery === this.castingTime) {
+      this.health += this.extraRecovery;
+      this.serialRecovery = 0;
+    }
+  }
+
+  isDead() {
+    return this.health <= 0;
+  }
+
+  capHealth() {
+    this.health = Math.min(this.health, this.maxHealth);
+  }
 }
 
-const initHelath = (health, maxHealth) => {
-  return health >= maxHealth ? maxHealth : health;
-};
-const recoveryHelath = (helath, recoveryAmount) => {
-  return helath + recoveryAmount;
-};
 
-const increaseSerialRecovery = (serialRecovery) => {
-  return serialRecovery + 1;
-};
 
-const checkIsDead = (health) => {
-  return health <= 0;
-};
-
-const resetSerialRecovery = () => {
-  return 0;
-};
-
-const damagedHelath = (helath, damage) => {
-  return helath - damage;
-};
-
-const canGetExtraRecovery = (serialRecovery, castingTime) => {
-  return serialRecovery === castingTime;
-};
