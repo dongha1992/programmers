f26889de3efa5b05078cb8c404a67afe0faaff80
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-01-31 16:23:59 +0900

    [level 1] Title: 공원 산책, Time: 0.65 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928?language=javascript) 
 
 ### 성능 요약
 
-메모리: 80.6 MB, 시간: 0.41 ms
+메모리: 32.4 MB, 시간: 0.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 17일 16:24:10
+2025년 01월 31일 16:23:56
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,90 +1,67 @@
-function solution(park, routes) {
-   
-const parks = park.map((p) => p.split(''));
-const routeArray = routes.map((r) => r.split(' '));
+const DIRECTION_MAP = {
+    E: [0, 1],
+    W: [0, -1],
+    S: [1, 0],
+    N: [-1, 0],
+  };
 
-const n = park.length;
-const m = routeArray.length;
 
-let currentPos = [];
+const solution = (park, routes) => {
+  park = park.map((p) => p.split(''));
 
-const checkColumn = (start, end, fixed) => {
-  let flag = false;
-  for (let l = start; l >= end; l--) {
-    if (parks[l][fixed] === 'X' || !parks[l][fixed]) {
-      flag = true;
-      break;
-    }
-  }
-  return flag;
-};
-  
-const checkRow = (start, end, fixed) => {
-  let flag = false;
-  for (let l = start; l >= end; l--) {
-    if (parks[fixed][l] === 'X' || !parks[fixed][l]) {
-      flag = true;
-      break;
-    }
-  }
-  return flag;
-};
+  const n = park.length;
+  const m = park[0].length;
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < parks[i].length; j++) {
-    if (parks[i][j] === 'S') {
-      currentPos = [i, j];
-      for (let k = 0; k < m; k++) {
-        const directions = routeArray[k];
-        const [direction, count] = directions;
-
-        if (direction === 'N') {
-          const nxI = currentPos[0] - Number(count);
-          const curJ = currentPos[1];
-          
-          const isLimit = nxI >= n || nxI < 0
-          if (isLimit || parks[nxI][curJ] === 'X' || checkColumn(currentPos[0], nxI, curJ)) {
-            continue;
-          }
-          currentPos = [nxI, curJ];
-        } else if (direction === 'S') {
-          const nxI = currentPos[0] + Number(count);
-          const curJ = currentPos[1];
+  let pos = getStartPos();
     
-          const isLimit = nxI >= n || nxI < 0
-          
-          if (isLimit || parks[nxI][curJ] === 'X' || checkColumn(nxI,currentPos[0], curJ)) {
-            continue;
-          }
-          currentPos = [nxI, curJ];
-        } else if (direction === 'W') {
-          const curI = currentPos[0];
-          const nxJ = currentPos[1] - Number(count);
-
-          const isLimit = nxJ >= m || nxJ < 0
-          
-          if (
-           isLimit || parks[curI][nxJ] === 'X' || checkRow(currentPos[1], nxJ, curI)
-          ) {
-            continue;
-          }
-          currentPos = [curI, nxJ];
-        } else if (direction === 'E') {
-          const curI = currentPos[0];
-          const nxJ = currentPos[1] + Number(count);
-          const isLimit = nxJ > m || nxJ < 0
-          const canCheck = currentPos[1] >= 0 && curI > 0 && nxJ > 0;
-          if (
-           isLimit|| parks[curI][nxJ] === 'X' ||
-         checkRow(nxJ,currentPos[1], curI)) {
-            continue;
-          }
-          currentPos = [curI, nxJ];
+  function getStartPos() {
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (park[i][j] === 'S') {
+          return new Position(i, j);
         }
       }
-      break;
     }
   }
+
+  function canMove(pos, dir, step) {
+    const tempPos = new Position(pos.x, pos.y);
+
+    for (let i = 0; i < step; i++) {
+      tempPos.move(DIRECTION_MAP[dir], 1);
+      if (tempPos.isLimit(n, m) || tempPos.isBlocked(park)) return false;
+    }
+    return true;
+  }
+
+  for (const route of routes) {
+    const routeInfo = route.split(' ');
+    const [dir, step] = routeInfo;
+    if (canMove(pos, dir, step)) {
+      pos.move(DIRECTION_MAP[dir], step);
+    }
+  }
+
+  return [pos.x, pos.y];
+};
+
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(direction, step) {
+    this.x += direction[0] * step;
+    this.y += direction[1] * step;
+  }
+
+  isLimit(r, c) {
+    return this.x < 0 || this.x >= r || this.y < 0 || this.y >= c;
+  }
+
+  isBlocked(board) {
+    return board[this.x][this.y] === 'X';
+  }
 }
-return currentPos;
-}
+
