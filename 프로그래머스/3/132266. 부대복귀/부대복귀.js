3a0703810ff719c57b2e83ec41b97ff38ea4881b
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-02-01 18:18:48 +0900

    [level 3] Title: 부대복귀, Time: 967.99 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,7 +1,7 @@
 function solution(n, roads, sources, destination) {
   const answer = [];
   const graph = {};
-  let visited = Array.from({ length: n+1 }).fill(-1);
+  const visited = Array.from({ length: n + 1 }).fill(-1);
 
   for (const [from, to] of roads) {
     if (!graph[from]) graph[from] = [];
@@ -13,28 +13,26 @@ function solution(n, roads, sources, destination) {
 
   const bfs = (destination) => {
     const queue = [[destination, 0]];
-    visited[destination] = 0
-    
+    visited[destination] = 0;
+
     while (queue.length) {
       const [n, d] = queue.shift();
-
       if (graph[n]) {
         for (const nn of graph[n]) {
-          if(visited[nn] < 0){
+          if (visited[nn] < 0) {
             queue.push([nn, d + 1]);
-            visited[nn] = d + 1
+            visited[nn] = d + 1;
           }
         }
-      } 
+      }
     }
   };
-  
-  bfs(destination)
-  
+
+  bfs(destination);
   for (let i = 0; i < sources.length; i++) {
     const dis = visited[sources[i]];
-    answer.push(dis)
+    answer.push(dis);
   }
 
-  return answer
+  return answer;
 }
