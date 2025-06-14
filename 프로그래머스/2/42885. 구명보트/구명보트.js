81890dc76ef4f4ec1c8cc1055a8f00b428912f27
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-14 14:22:10 +0900

    [level 2] Title: 구명보트, Time: 33.71 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -3,19 +3,19 @@ function solution(people, limit) {
   let rt = people.length-1
   let answer = 0;
   people.sort((a, b) => a-b)
-  while(lt <= rt){
-    const low = people[lt]
-    const high = people[rt]
     
-    if(low+high <= limit){
-      lt++
-      rt--
-      answer++
-    } else {
-      rt--
-      answer++
+  while(lt <= rt){
+    const low = people[lt];
+    const high = people[rt];
+    
+    if (low + high <= limit) {
+      lt++;
     }
+    rt--;
+    answer++;
   }
 
   return answer
 }
+
+
