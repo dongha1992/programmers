2e38fbd90124e1af285499649e700b32d290a57b
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-15 22:07:44 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.92 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,36 +1,41 @@
 function solution(str1, str2) {
-let n = 2;
+    const arr1 = splitToBigrams(str1);
+    const arr2 = splitToBigrams(str2);
 
- const helper = (str) => {
-   let tmp = []
-   let cnt = 0
-    while(true){
-     const sub = str.substr(cnt, 2)
-     if(!/[\{\}\[\];:<>.,\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g.test(sub) && !/[0-9]/g.test(sub) &&!/\s/g.test(sub) && sub.length > 1){
-     tmp.push(sub.toLowerCase()) 
-     } 
-     cnt++
-     if(cnt >= str.length) break;
-  }
-   return tmp
- }
- 
- const s1 = helper(str1)
- const s2 = helper(str2)
- 
- 
- if(s1.length===0 && s2.length===0) return 65536
-  console.log(s1, s2)
- let diff = 0;
- let union = 0;
- for(let i =0; i < s1.length;i++){
-   if(s2.indexOf(s1[i])!==-1){
-     diff++
-     s2.splice(s2.indexOf(s1[i]), 1)
-   }
-   union++
- }
-  union+=s2.length
- const answer = (diff / union)
- return !isNaN(answer) ? parseInt((answer) * 65536) : 65536
+    if (arr1.length === 0 && arr2.length === 0) return 65536;
+
+    const freqMap1 = createMap(arr1);
+    const freqMap2 = createMap(arr2);
+
+    let intersection = 0;
+    let union = 0;
+    const allKeys = new Set([...freqMap1.keys(), ...freqMap2.keys()]);
+
+    allKeys.forEach(key => {
+        const cnt1 = freqMap1.get(key) || 0;
+        const cnt2 = freqMap2.get(key) || 0;
+        intersection += Math.min(cnt1, cnt2);
+        union += Math.max(cnt1, cnt2);
+    });
+
+    const jaccard = union === 0 ? 1 : intersection / union;
+    return Math.floor(jaccard * 65536);
+}
+
+function splitToBigrams(str) {
+    const upper = str.toUpperCase();
+    const bigrams = [];
+    
+    for (let i = 0; i < upper.length - 1; i++) {
+        const chunk = upper.slice(i, i + 2);
+        if (/^[A-Z]{2}$/.test(chunk)) {
+            bigrams.push(chunk);
+        }
+    }
+    return bigrams;
+}
+
+function createMap(arr) {
+    return arr.reduce((map, key) => 
+        map.set(key, (map.get(key) || 0) + 1), new Map());
 }
