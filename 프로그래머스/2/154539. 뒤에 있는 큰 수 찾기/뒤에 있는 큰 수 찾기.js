92e255dfe5e6a3ee6fad6ac4b70b10d339ed8abe
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-16 01:37:38 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 82.72 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,56 +1,20 @@
-// function solution(numbers) {
-//   const answer = Array.from({length:numbers.length},()=>-1)
-//   const stack = [];
-    
-//   for(let i = 0; i < numbers.length; i++) {
-//     while(stack.length && numbers[stack[stack.length-1]] < numbers[i]){
-//         const index = stack.pop();
-//         answer[index] = numbers[i];
-//     }
-//       stack.push(i)
-//   }
-//   return answer
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function solution(numbers) {
-    const answer = Array.from({length:numbers.length}, () => -1)
-    const stack = []
-    
-    for(let i = numbers.length - 1; i >= 0; i--) {
-        const curr = numbers[i];
-        
-        while(stack.length && stack[stack.length-1] <= curr) {
-            stack.pop()
-        }
-        
-        if (stack.length) {
-            answer[i] = stack[stack.length - 1];
-        }
+  const answer = Array.from({ length: numbers.length }, () => -1);
+  const stack = [];
 
-        stack.push(curr)
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    const curr = numbers[i];
+
+    while (stack.length && stack[stack.length - 1] <= curr) {
+      stack.pop();
     }
 
-    
-    return answer
+    if (stack.length) {
+      answer[i] = stack[stack.length - 1];
+    }
+
+    stack.push(curr);
+  }
+
+  return answer;
 }
