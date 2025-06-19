73cf38296459dacaeef1ee1afd1380910151090e
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-19 22:32:49 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 36.35 ms, Memory: 55.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -0,0 +1,42 @@
+function solution(diffs, times, limit) {
+  const puzzle_process = (level) => {
+    let totalTime = 0;
+
+    for (let i = 0; i < diffs.length; i++) {
+      const diff = (diffs[i]);
+      const time = (times[i]);
+      const prevTime = i > 0 ? (times[i - 1]) : 0;
+
+      if (diff > (level)) {
+        const failTime = diff - (level);
+        const retryTime = failTime * (time + prevTime) + time;
+        totalTime += retryTime;
+      } else {
+        totalTime += time;
+      }
+
+      if (totalTime > (limit)) return totalTime;
+    }
+
+    return totalTime;
+  };
+
+  let low = 1;
+  let high = 100000
+  let answer = high;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const totalTime = puzzle_process(mid);
+
+    if (totalTime <= limit) {
+      answer = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
