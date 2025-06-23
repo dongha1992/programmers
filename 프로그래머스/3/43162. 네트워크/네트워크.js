23eeff5359931285ba5d7ea369a87c3d3528c0e6
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-23 16:43:47 +0900

    [level 3] Title: 네트워크, Time: 0.71 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,38 @@
 function solution(n, computers) {
-  const ch = Array.from({length:n+1}, ()=>0)
-   let answer = 0;
-  
-    const dfs = (v) => {
-     for(let i = 1; i <= n; i++) {
-        if(computers[v][i]===1 && ch[i]===0){
-          ch[i]=1
-          dfs(i)
-        }
+  const graph = {};
+  const visited = Array.from({ length: n + 1 }).fill(false);
+
+  for (let i = 0; i < computers.length; i++) {
+    graph[i + 1] = [];
+    for (let j = 0; j < computers[0].length; j++) {
+      if (computers[i][j] === 1 && i !== j) {
+        graph[i + 1].push(j + 1);
+      }
     }
   }
+
+  let count = 0;
     
-  for(let i = 0; i < n; i++){
-    if(ch[i]===0){
-      dfs(i)
-      answer++
+  for (let i = 1; i <= n; i++) {
+    if (visited[i]) continue;
+
+    const queue = [i];
+    visited[i] = true;
+      
+    while (queue.length) {
+      const node = queue.shift();
+
+      for (const nx of graph[node]) {
+        if (!visited[nx]) {
+          queue.push(nx);
+          visited[nx] = true;
+        }
+      }
     }
+      count++
   }
-  return answer
+  return count
 }
+
+
+
