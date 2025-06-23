5241e09352573940b7dec8a493be2cdeef5d7ce3
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-23 19:23:52 +0900

    [level 3] Title: 야근 지수, Time: 818.59 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,18 +1,31 @@
 function solution(n, works) {
-  if (works.reduce((a, b) => a + b, 0) <= n) return 0;
   works.sort((a, b) => b - a);
     
-  while(n){
-    works[0]--;
-    n--
-    
+  while (n) {
+      
     let i = 0;
-    while(works[i] < works[i+1]){
-       [works[i], works[i + 1]] = [works[i + 1], works[i]];
-        i++;
+    while(works[i] < works[i+1]) {
+        [works[i] , works[i+1]] = [works[i+1] , works[i]];
+        i++
     }
+        
+    if (!works[0]) break;
+    works[0]--;
+    n--;
   }
-  
-  return works.reduce((acc, cur) => acc + Math.pow(cur, 2),0)
- }
+
+  return works.reduce((acc, cur) => acc + Math.pow(cur, 2), 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
