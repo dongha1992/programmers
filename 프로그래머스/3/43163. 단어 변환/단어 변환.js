31d4f2efddb9b52c57d6bca0989c8667b47f4630
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-23 20:15:12 +0900

    [level 3] Title: 단어 변환, Time: 0.74 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,48 +1,37 @@
 function solution(begin, target, words) {
-  let answer = 0;
-  const ch = Array.from({length:words.length}).fill(0)
-  
-  if(!words.includes(target)) return 0
-  
-  const bfs = (word) => {
-    const queue = [];
-    queue.push([word, 0])
-    
-    while(queue.length){
-      let [cur, step] = queue.shift();
-      const cnt = diffBetweenWord(cur, target);
+  const n = words.length;
+  const visited = Array.from({ length: n }).fill(false);
+
+  const queue = [[begin, 0]];
+
+  while (queue.length) {
+    const [word, step] = queue.shift();
       
-      if(cur === target) {
-         answer = step
-         return
-       }
+    if (word === target) return step;
       
-      for(let i = 0; i < words.length; i++){
-        if(ch[i] === 0){
-          const word = words[i]
-          const cnt = diffBetweenWord(cur, word);
-          if(cnt === 1){
-            step++;
-            queue.push([word, step]);
-            ch[i] = 1;
-          } 
-        }
+    for (let i = 0; i < words.length; i++) {
+      if (visited[i]) continue;
+
+      if (compareWord(word, words[i])) {
+        visited[i] = true;
+        queue.push([words[i], step + 1]);
       }
-    }     
+    }
   }
-  bfs(begin)
-  return answer
+
+  function compareWord(word1, word2) {
+    let arr = [...word2];
+
+    for (let i = 0; i < word1.length; i++) {
+      const index = arr.indexOf(word1[i]);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+    }
+
+    return arr.length === 1;
+  }
+
+  return 0
 }
 
-  
-function diffBetweenWord(str1, str2) {
-  let cnt = 0;
-  const minLength = Math.min(str1.length, str2.length);
-  
-  for(let i = 0; i < minLength; i++){
-    if(str1[i] !== str2[i]) cnt++;
-  }
-  
-  cnt += Math.abs(str1.length - str2.length);
-  return cnt;
-}
