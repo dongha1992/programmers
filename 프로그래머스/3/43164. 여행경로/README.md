4851da6c3e979541bdfa8157516e43876fbb1243
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-24 15:12:15 +0900

    [level 3] Title: 여행경로, Time: 279.26 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 33.2 MB, 시간: 0.12 ms
+메모리: 60.2 MB, 시간: 279.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 19일 13:18:52
+2025년 06월 24일 15:12:12
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,28 +1,32 @@
 function solution(tickets) {
- const n = tickets.length
- const finish = tickets.length + 1
- const ch = Array.from({length:n}, ()=>0)
- const answer = []
- 
- const dfs = (route) => {
-   if(route.length === finish){
-     answer.push(route)
-   } else {
-     for(let i = 0; i < tickets.length; i++){
-       if(!ch[i]){
-         const [start, end] = tickets[i]
-         if(route[route.length-1] === start){
-            ch[i] = 1
-            dfs([...route, end])
-            ch[i] = 0 
-         }
-       }
-     }
-   }
- }
- dfs(["ICN"])
-  return answer.sort()[0]
+  const n = tickets.length;
+  const visited = Array.from({ length: n }).fill(false);
+  const answer = [];
+
+  const dfs = (depth, path) => {
+    if (depth === n) {
+      answer.push(path);
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (!visited[i]) {
+        const [start, end] = tickets[i];
+
+        if (path[path.length - 1] === start) {
+          visited[i] = true;
+          dfs(depth + 1, [...path, end]);
+          visited[i] = false;
+        }
+      }
+    }
+  };
+  dfs(0, ['ICN']);
+  return answer.sort()[0];
 }
+
+
+
 
 
 
