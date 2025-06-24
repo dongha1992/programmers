f8d831d6d9d0614ba1807859950e1a8749bc4c69
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-24 17:55:17 +0900

    [level 3] Title: 섬 연결하기, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,41 +1,51 @@
 function solution(n, costs) {
-  const unionFind = new UnionFind(n);
-  costs.sort((a, b) => a[2] - b[2]); 
-  let answer = 0;
-  
-  for(const [u, v, w] of costs){
-    if(unionFind.union(u, v)){
-      answer += w
-
-    }
-  }
-  return answer
-}
-
-
-class UnionFind {
-    constructor(n) {
-        this.parent = new Array(n).fill(-1);
-    }
-
-    find(x) {
-        if (this.parent[x] < 0) return x;
-        return this.parent[x] = this.find(this.parent[x]);
-    }
-
-    union(x, y) {
-        let rootX = this.find(x);
-        let rootY = this.find(y);
-
-        if (rootX === rootY) return false;
-
-        if (this.parent[rootX] > this.parent[rootY]) {
-            [rootX, rootY] = [rootY, rootX];
+    const graph = Array.from({ length: n }, () => []);
+    
+    for(const [u, v, w] of costs) {
+        graph[u].push({to: v, w})
+        graph[v].push({to: u, w})
+      }
+    const visited = Array(n).fill(false);
+    const minEdge = Array(n).fill(Infinity);
+    const parent = Array(n).fill(-1);
+    
+    minEdge[0] = 0;
+    let totalWeight = 0
+    const mst = []
+    
+    for(let i = 0; i < n; i++) {
+        let u = -1;
+        for(let v = 0; v < n; v++) {
+            if(!visited[v] && (u === -1 ||  minEdge[v] < minEdge[u])) {
+                u = v
+            }
         }
-
-        this.parent[rootX] += this.parent[rootY];
-        this.parent[rootY] = rootX;
-
-        return true;
+        
+        if(minEdge[u] === Infinity) break;
+        
+        visited[u] = true;
+        totalWeight += minEdge[u]
+        
+        if(parent[u] !== -1) {
+         mst.push([parent[u], u, minEdge[u]])
+        }
+        
+        for(const {to, w} of graph[u]) {
+            if(!visited[to] && w < minEdge[to]) {
+                minEdge[to] = w
+                parent[to] = u;
+            }
+        }
     }
+    
+    return totalWeight 
+    
 }
+
+
+
+
+
+
+
+
