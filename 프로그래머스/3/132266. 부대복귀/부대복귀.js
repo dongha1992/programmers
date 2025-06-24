47da66e5e2bfeb0922fdd2bd316c4dfc4a001f4d
김동하 <58624851+dongha1992@users.noreply.github.com>
2025-06-24 16:11:41 +0900

    [level 3] Title: 부대복귀, Time: 9568.15 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,7 +1,48 @@
+// function solution(n, roads, sources, destination) {
+//   const answer = [];
+//   const graph = {};
+//   const visited = Array.from({ length: n + 1 }).fill(-1);
+
+//   for (const [from, to] of roads) {
+//     if (!graph[from]) graph[from] = [];
+//     graph[from].push(to);
+
+//     if (!graph[to]) graph[to] = [];
+//     graph[to].push(from);
+//   }
+
+//   const bfs = (destination) => {
+//     const queue = [[destination, 0]];
+//     visited[destination] = 0;
+
+//     while (queue.length) {
+//       const [n, d] = queue.shift();
+//       if (graph[n]) {
+//         for (const nn of graph[n]) {
+//           if (visited[nn] < 0) {
+//             queue.push([nn, d + 1]);
+//             visited[nn] = d + 1;
+//           }
+//         }
+//       }
+//     }
+//   };
+
+//   bfs(destination);
+//   for (let i = 0; i < sources.length; i++) {
+//     const dis = visited[sources[i]];
+//     answer.push(dis);
+//   }
+
+//   return answer;
+// }
+
+
 function solution(n, roads, sources, destination) {
   const answer = [];
   const graph = {};
-  const visited = Array.from({ length: n + 1 }).fill(-1);
+
+  const dist = Array.from({ length: n + 1 }).fill(-1);
 
   for (const [from, to] of roads) {
     if (!graph[from]) graph[from] = [];
@@ -11,28 +52,29 @@ function solution(n, roads, sources, destination) {
     graph[to].push(from);
   }
 
-  const bfs = (destination) => {
-    const queue = [[destination, 0]];
-    visited[destination] = 0;
+  const queue = [];
+  queue.push([destination, 0])
+  dist[destination] = 0;
+    
+  while (queue.length) {
+    const [node, w] = queue.shift();
 
-    while (queue.length) {
-      const [n, d] = queue.shift();
-      if (graph[n]) {
-        for (const nn of graph[n]) {
-          if (visited[nn] < 0) {
-            queue.push([nn, d + 1]);
-            visited[nn] = d + 1;
-          }
-        }
+    if(graph[node]) {
+         for (const nx of graph[node]) {
+      if (dist[nx] < 0) {
+        queue.push([nx, w + 1]);
+        dist[nx] = w + 1 
       }
+    }   
     }
-  };
-
-  bfs(destination);
-  for (let i = 0; i < sources.length; i++) {
-    const dis = visited[sources[i]];
-    answer.push(dis);
   }
 
+  for (let i = 0; i < sources.length; i++) {
+      const d = dist[sources[i]]
+      answer.push(d)
+  }
   return answer;
 }
+
+
+
