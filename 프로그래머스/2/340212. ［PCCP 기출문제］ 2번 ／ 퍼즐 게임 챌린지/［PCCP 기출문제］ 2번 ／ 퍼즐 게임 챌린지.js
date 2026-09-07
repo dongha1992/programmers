a8d2a0ff849bc02ca12ae45472542a38db57041e
김동하 <58624851+dongha1992@users.noreply.github.com>
2026-09-07 14:11:14 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 47.70 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,37 +1,48 @@
 function solution(diffs, times, limit) {
+    // diff <= level time_cur 만큼 소요시간
+    // diff > level 이면 diff - level 번 틀림 (1회당 time_cur + time_prev)
+       // diff - level 틀린 후 time_cur 사용해서 품
+    
+    // 모두 해결하기 위한 숙련도(level)의 최소값
+    
+    let low = 0;
+    let high = 100000;
     let answer = 0;
     
-    const getTotalTime = (level) => {
-        let result = 0;
+    while(low <= high) {
+        const mid = Math.floor((low + high) / 2); // level;
+        let total_time = 0;
         
         for(let i = 0; i < diffs.length; i++) {
-            const diff = diffs[i]
-            const prevTime = i > 0 ? times[i - 1] : 0;
+            const time_cur = times[i];
+            const time_prev = times[i-1];
+            const diff = diffs[i];
             
-            if(diff <= level) {
-                result += times[i]
+            if(diff <= mid) {
+                total_time += time_cur
             } else {
-                const repeat = diff - level
-                result += repeat * (prevTime+times[i]) + times[i]
+                total_time += diffIsGreaterThanLevel(diff, mid, time_cur, time_prev)
             }
+            
+            if(total_time >= limit) {
+                break;
+            } 
         }
-        return result
-    }
-    
-    let high = 100000
-    let low = 1;
-    
-    while(low <= high) {
-        const mid = Math.floor((high + low) / 2);
-        const totalTime = getTotalTime(mid)
         
-        if(totalTime <= limit) {
-            answer = mid;
-            high = mid -1
+       if(total_time <= limit) {
+        // 성공했다면
+        high = mid - 1
+        answer = mid;
         } else {
-            low = mid +1 
+         // 실패 → 숙련도 높여야 함
+         low = mid + 1;
         }
     }
+    
     return answer;
 }
 
+const diffIsGreaterThanLevel = (diff, level, time_cur, time_prev) => {
+    const d = diff - level;
+    return d * (time_cur + time_prev) + time_cur
+}
